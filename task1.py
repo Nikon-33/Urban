36a9307c30966370d35708c2ab031baef661497b
Nikon-33 <89101717800@mail.ru>
2024-06-27 15:23:36 +0300

    Add files via upload

---
+++ task1.py
@@ -0,0 +1,12 @@
+# 1st program
+print(9 ** 0.5 * 5)
+# 2st program
+print(9.99 > 9.98 and 1000 != 1000.1)
+# 3ed program
+num1 = "1234"
+num2 = "5678"
+print((int(num1[1:3]) + int(num2[1:3])))
+# 4th program
+num1 = "13.42"
+num2 = "42.13"
+print((int(num1[0:2])) == (int(num2[3:])) or (int(num1[3:])) == (int(num2[0:2])))
